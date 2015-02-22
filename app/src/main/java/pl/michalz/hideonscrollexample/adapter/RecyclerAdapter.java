e1ce9c2a64ae5d8ba143154f8665265e9464c06b
boblablaw <jasondavidbeatty@gmail.com>
2015-02-22 17:09:32 -0500

    Updated to make it simpler

--- app/src/main/java/pl/michalz/hideonscrollexample/adapter/RecyclerAdapter.java
+++ app/src/main/java/pl/michalz/hideonscrollexample/adapter/RecyclerAdapter.java
@@ -1,23 +1,16 @@
 package pl.michalz.hideonscrollexample.adapter;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import pl.michalz.hideonscrollexample.R;
-import pl.michalz.hideonscrollexample.adapter.viewholder.RecyclerHeaderViewHolder;
-import pl.michalz.hideonscrollexample.adapter.viewholder.RecyclerItemViewHolder;
 
 import java.util.List;
 
-/*
-* RecyclerView Adapter that allows to add a header view.
-* */
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+import pl.michalz.hideonscrollexample.R;
+import pl.michalz.hideonscrollexample.adapter.viewholder.RecyclerItemViewHolder;
 
-    private static final int TYPE_HEADER = 2;
-    private static final int TYPE_ITEM = 1;
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<String> mItemList;
 
     public RecyclerAdapter(List<String> itemList) {
@@ -26,47 +19,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        if (viewType == TYPE_ITEM) {
-            final View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
-            return RecyclerItemViewHolder.newInstance(view);
-        } else if (viewType == TYPE_HEADER) {
-            final View view = LayoutInflater.from(context).inflate(R.layout.recycler_header, parent, false);
-            return new RecyclerHeaderViewHolder(view);
-        }
-        throw new RuntimeException("There is no type that matches the type " + viewType + " + make sure your using types correctly");
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return RecyclerItemViewHolder.newInstance(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-        if (!isPositionHeader(position)) {
-            RecyclerItemViewHolder holder = (RecyclerItemViewHolder) viewHolder;
-            String itemText = mItemList.get(position - 1); // header
-            holder.setItemText(itemText);
-        }
-    }
-
-    public int getBasicItemCount() {
-        return mItemList == null ? 0 : mItemList.size();
-    }
-
-
-    @Override
-    public int getItemViewType(int position) {
-        if (isPositionHeader(position)) {
-            return TYPE_HEADER;
-        }
-
-        return TYPE_ITEM;
+        RecyclerItemViewHolder holder = (RecyclerItemViewHolder) viewHolder;
+        String itemText = mItemList.get(position);
+        holder.setItemText(itemText);
     }
 
     @Override
     public int getItemCount() {
-        return getBasicItemCount() + 1; // header
-    }
-
-    private boolean isPositionHeader(int position) {
-        return position == 0;
+        return mItemList == null ? 0 : mItemList.size();
     }
 
 }
